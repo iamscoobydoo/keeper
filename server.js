e31c580c4b550af7c31680646f37f75316d22907
iamscoobydoo <arka.bhattacharya33@gmail.com>
2021-04-12 14:15:52 +0530

    add user route

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json({ extended: false }));
 
 //define routes
 app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/user", require("./routes/api/user"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
